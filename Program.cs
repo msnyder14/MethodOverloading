1ca18f390691ac81c541460c6a5cb4c3ecfc5d62
Mark Snyder <157758808+msnyder14@users.noreply.github.com>
2025-01-16 14:44:57 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MethodOverloading
+namespace MethodOverloading
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            PrintName("Mark Snyder");
+            PrintName("Snyder", "Mark");
+            PrintName("Snyder", "Mark", "Edward", "Orelius", "Crysanthemum");
+        }
+
+        static void PrintName(string name)
+        {
+            Console.WriteLine("Name: {name}");
+        }
+
+        static void PrintName(string firstname, string lastname)
+        {
+            Console.WriteLine("Name: {lastname}, {firstname}");
+        }
+
+        static void PrintName(string firstname, string lastname, params string[] othernames)
+        {
+            Console.Write("Name: {lastname}, {firstname}");
+            foreach (var name in othernames) { Console.Write(", {name}"); }
+            Console.WriteLine();
+        }
+
+        static int SumOfProducts(params int[] values)
+        {
+            return 0;
         }
     }
 }
